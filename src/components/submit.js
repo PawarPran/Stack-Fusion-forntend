a9d215202d7b82d1d79eaa9bd5b77ebfb62d021e
Pranali Pawar <pranalipawar555@gmail.com>
2023-02-02 20:28:42 +0530

    first commit

---
+++ src/components/submit.js
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+
+function Submit(props){
+ 
+return(
+    <>
+    
+    <button type="submit" id="button" >Submit</button>
+    </>
+)
+}
+export default Submit;
